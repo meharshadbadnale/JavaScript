9be31cfb972f7bcba424e269ba750725353e0a33
Harshad Badnale <138968429+meharshadbadnale@users.noreply.github.com>
2025-01-31 12:27:23 +0530

    Add files via upload

---
+++ conditionalstatement.js
@@ -0,0 +1,38 @@
+//if statement
+let mode = "light";
+let color;
+
+if(mode === "dark") {
+    color = "Black";
+}
+if(mode === "light") {
+    color = "White";
+}
+console.log(color); 
+//White
+
+
+//if-else statement
+let age = 23;
+
+if(age > 18){
+    console.log("you can vote");
+}else{
+    console.log("you cannot vote");
+} 
+//you can vote
+
+
+//else-if statement
+let age = 89;
+
+if(age < 18) {
+    console.log("junior");
+}else if(age > 60){
+    console.log("senior");
+}else{
+    console.log("middle");
+}
+//senior
+
+
